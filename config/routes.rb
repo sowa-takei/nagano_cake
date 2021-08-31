932a59340316079d4527603d2ee8cd9dfece22c2
sowa-takei <souwasora@gmail.com>
2021-08-31 03:30:28 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
   devise_for :admins
   get 'admin' => "admin/homes#top", as: "admin_top"
+
   namespace :admin do
-  resources :products, only: [:new, :index, :create, :show, :edit, :update]
-  resources :genres, only: [:index, :create, :edit, :update]
-  resources :customers, only: [:index, :show, :edit, :update]
+   resources :products, only: [:new, :index, :create, :show, :edit, :update]
+   resources :genres, only: [:index, :create, :edit, :update]
+   resources :customers, only: [:index, :show, :edit, :update]
+  end
+
+ namespace :public do
+ get 'home/about' => "homes#about", as: "home_about"
+ get 'home/about' => "homes#about", as: "home_about"
  end
+
+ root to: 'public/homes#top'
+ get 'home/about' => "homes#about"
+
 end
